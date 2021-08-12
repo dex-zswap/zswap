1de8299cca1598651d846f7a55255cb60bcd48e0
rwson <rw_song@sina.com>
2021-08-12 10:04:03 +0800

    feat: Add Pool hook

--- src/views/Pool/hooks/index.ts
+++ src/views/Pool/hooks/index.ts
@@ -18,57 +18,52 @@ export function useUserPairs() {
   const { account } = useActiveWeb3React()
   const factoryContract = useFactoryContract()
 
-  // const allPairsLength = useSingleCallResult(factoryContract, 'allPairsLength')
-  // const pairsIndexArgs = useMemo<Array<Array<number>>>(() => {
-  //   const indexs: Array<Array<number>> = []
-  //   const { length } = allPairsLength.result ?? []
-  //   for (let i = 0; i < length; i++) {
-  //     indexs.push([i])
-  //   }
-  //   return indexs
-  // }, [allPairsLength])
+  const allPairsLength = useSingleCallResult(factoryContract, 'allPairsLength')
+  const pairsIndexArgs = useMemo<Array<Array<number>>>(() => {
+    const indexs: Array<Array<number>> = []
+    const { length } = allPairsLength.result ?? []
+    for (let i = 0; i < length; i++) {
+      indexs.push([i])
+    }
+    return indexs
+  }, [allPairsLength])
 
-  // const allPairs = useSingleContractMultipleData(factoryContract, 'allPairs', pairsIndexArgs)
-  // const allPairsIdArgs = useMemo(() => {
-  //   return allPairs.map((pairResult) => pairResult.result?.[0] ?? null)
-  // }, [allPairs])
+  const allPairs = useSingleContractMultipleData(factoryContract, 'allPairs', pairsIndexArgs)
+  const allPairsIdArgs = useMemo(() => {
+    return allPairs.map((pairResult) => pairResult.result?.[0] ?? null)
+  }, [allPairs])
 
-  console.log(account)
+  const allBalanceOf = useMultipleContractSingleData(allPairsIdArgs, PAIR_INTERFACE, 'balanceOf', [account])
+  const allTotalSupply = useMultipleContractSingleData(allPairsIdArgs, PAIR_INTERFACE, 'totalSupply', [])
+  const allToken0 = useMultipleContractSingleData(allPairsIdArgs, PAIR_INTERFACE, 'token0', [])
+  const allToken1 = useMultipleContractSingleData(allPairsIdArgs, PAIR_INTERFACE, 'token1', [])
 
-  // const allBalanceOf = useMultipleContractSingleData(allPairsIdArgs, PAIR_INTERFACE, 'balanceOf', [account])
-  // const allTotalSupply = useMultipleContractSingleData(allPairsIdArgs, PAIR_INTERFACE, 'totalSupply', [])
-  // const allToken0 = useMultipleContractSingleData(allPairsIdArgs, PAIR_INTERFACE, 'token0', [])
-  // const allToken1 = useMultipleContractSingleData(allPairsIdArgs, PAIR_INTERFACE, 'token1', [])
+  console.log(allBalanceOf, allPairs, allPairsLength)
 
-  // console.log(allBalanceOf, allPairs, allPairsLength)
+  const pairsToken = useMemo(() => {
+    const tokens = []
 
-  // const pairsToken = useMemo(() => {
-  //   const tokens = []
+    let totalSupply, token0, token1
 
-  //   let totalSupply, token0, token1
+    allBalanceOf.forEach(({ result: balanceOf }, idx) => {
+      totalSupply = allTotalSupply[idx].result
+      token0 = allToken0[0].result
+      token1 = allToken1[0].result
 
-  //   allBalanceOf.forEach(({ result: balanceOf }, idx) => {
-  //     totalSupply = allTotalSupply[idx].result
-  //     token0 = allToken0[0].result
-  //     token1 = allToken1[0].result
+      if (balanceOf && balanceOf.length > 0) {
+        tokens.push({
+          balanceOf: new Percent(
+            JSBI.BigInt(balanceOf[0].toNumber()),
+            JSBI.BigInt(allTotalSupply[idx].result[0].toNumber()),
+          ),
+          // token0: alltoken0[idx].result[0],
+          // token1: alltoken1[idx].result[0],
+        })
+      }
+    })
 
-
-  //     if (result && result.length > 0) {
-  //       if (result.length && result[0].gte(0)) {
-  //         tokens.push({
-  //           balanceOf: new Percent(
-  //             JSBI.BigInt(result[0].toNumber()),
-  //             JSBI.BigInt(allTotalSupply[idx].result[0].toNumber()),
-  //           ),
-  //           token0: alltoken0[idx].result[0],
-  //           token1: alltoken1[idx].result[0],
-  //         })
-  //       }
-  //     }
-  //   })
-
-  //   return tokens
-  // }, [allBalanceOf, allTotalSupply, allPairsIdArgs, allToken0, allToken1])
+    return tokens
+  }, [allBalanceOf, allTotalSupply, allPairsIdArgs, allToken0, allToken1])
 
   // const pairContracts = usePairContracts(allPairsIdArgs)
 
