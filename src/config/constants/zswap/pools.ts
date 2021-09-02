981e3448dddbe520ed145eff2238a2b16f3c96f1
rwson <rw_song@sina.com>
2021-09-02 15:11:17 +0800

    add wdex

--- src/config/constants/zswap/pools.ts
+++ src/config/constants/zswap/pools.ts
@@ -1,13 +1,21 @@
 import { PoolConfig, PoolCategory } from 'config/constants/types'
 
-import { ZSWAP_DEX_ADDRESS, ZSWAP_ZB_ADDRESS, ZSWAP_ZBST_ADDRESS, ZSWAP_STAKE_ADDRESS } from './address'
+import { ZSWAP_DEX_ADDRESS, ZSWAP_ZB_ADDRESS, ZSWAP_ZBST_ADDRESS, ZSWAP_STAKE_ADDRESS, ZSWAP_ZERO_ADDRESS } from './address'
 import EARNING_TOKEN from './earing-token'
 
 const tokens = {
+  wdex: {
+    symbol: 'WDEX',
+    address: {
+      3603102: ZSWAP_DEX_ADDRESS,
+    },
+    decimals: 18,
+    projectLink: 'https://pancakeswap.finance/',
+  },
   dex: {
     symbol: 'DEX',
     address: {
-      3603102: ZSWAP_DEX_ADDRESS,
+      3603102: ZSWAP_ZERO_ADDRESS,
     },
     decimals: 18,
     projectLink: 'https://pancakeswap.finance/',
@@ -31,6 +39,19 @@ const tokens = {
 }
 
 const pools: PoolConfig[] = [
+  {
+    sousId: 8,
+    stakingToken: tokens.wdex,
+    earningToken: EARNING_TOKEN,
+    contractAddress: {
+      3603102: ZSWAP_STAKE_ADDRESS,
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    tokenPerBlock: '10',
+    sortOrder: 1,
+    isFinished: false,
+  },
   {
     sousId: 9,
     stakingToken: tokens.dex,
