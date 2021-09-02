2b39bfd8fd6462aee1e35e39dc1baccd647d2a10
rwson <rw_song@sina.com>
2021-09-02 17:22:54 +0800

    fixed lp staked user percent

--- src/hooks/useContractCall.ts
+++ src/hooks/useContractCall.ts
@@ -3,55 +3,6 @@ import { useState, useEffect, useMemo } from 'react'
 import useRefresh from 'hooks/useRefresh'
 import { Contract } from '@ethersproject/contracts'
 
-type ResultType = {
-  loading: boolean
-  error: boolean
-  result: unknown
-}
-
-const defaultState: ResultType = {
-  loading: true,
-  error: false,
-  result: null,
-}
-
-function callContract(contract: Contract, methodName: string, inputs: string[], isMutil: boolean = false) {
-  const [results, setResults] = useState(isMutil ? new Array(inputs.length).fill(defaultState) : defaultState)
-  const method = contract.methods.lp_weight
-
-  useEffect(() => {
-    const fetchResult = async (input: unknown[]) => {
-      const result = defaultState
-      try {
-        const res = await method(...input).call()
-        return Object.assign({}, result, {
-          loading: false,
-          result: res,
-        })
-      } catch (e) {
-        return Object.assign({}, result, {
-          loading: false,
-          error: true,
-        })
-      }
-    }
-
-    const fetchResults = async () => {
-      const callQueue = isMutil
-        ? (inputs as Array<string>).map((input) => fetchResult([input]))
-        : [fetchResult([inputs])]
-      const results = await Promise.all(callQueue)
-      if (results.length === inputs.length) {
-        setResults(isMutil ? results : results[0])
-      }
-    }
-
-    fetchResults()
-  }, [method, inputs, isMutil])
-
-  return results
-}
-
 export function useContractCall(contract: Contract | null | any, methodName: string, inputs: Array<unknown> = []) {
   const { slowRefresh } = useRefresh()
   const [result, setResult] = useState(null)
