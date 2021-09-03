495ecd40252dca8e50aeb49439e7114364690db0
wyx2333333 <15850546725@163.com>
2021-09-03 17:30:18 +0800

    Delete console

--- src/state/mint/hooks.ts
+++ src/state/mint/hooks.ts
@@ -186,7 +186,6 @@ export function useDerivedMintInfo(
         const dependentCurrency = dependentField === Field.CURRENCY_B ? currencyB : currencyA
         const price = pair.priceOf(dependentField === Field.CURRENCY_B ? tokenA : tokenB)
         const dependentTokenAmount = price.quote(wrappedIndependentAmount)
-        console.log(price.toFixed(100))
         return dependentCurrency === ETHER ? CurrencyAmount.ether(dependentTokenAmount.raw) : dependentTokenAmount
       }
       return undefined
