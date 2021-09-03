07606e3f4a89ff3272a6bc45a9235a000af1f61e
wyx2333333 <15850546725@163.com>
2021-09-03 10:47:27 +0800

    Prettier

--- src/config/constants/zswap/online-time.ts
+++ src/config/constants/zswap/online-time.ts
@@ -1,5 +1,5 @@
 const isBuild = ['dev', 'test'].includes(process.env.BUILD_TYPE)
-const ONLINE_TIME = +(new Date('2021-08-25 18:00:00'))
+const ONLINE_TIME = +new Date('2021-08-25 18:00:00')
 
 export const DATE_SECS = 24 * 60 * 60 * 1000
 
